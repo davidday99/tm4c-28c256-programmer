fb31abebca4f63be997445c2f10faba83f8298bf
David Day <david.day2017@gmail.com>
2023-06-14 02:52:49 -0500

    update parameter names

--- src/app/rpc.c
+++ src/app/rpc.c
@@ -1,6 +1,25 @@
+#include <stddef.h>
 #include "app/rpc.h"
 #include "app/rpc_server.h"
 #include "mems/mem.h"
 
-void rpc_ReadCommand(struct rpc_RequestPacket rq) {
+void rpc_ReadCommand(struct mem_Device *mem,
+                    void *arg,
+                    unsigned long int addr,
+                    char *buf,
+                    size_t count) {
+    mem->read(arg, addr, buf, count);
 }
+
+void rpc_WriteCommand(struct mem_Device *mem,
+                    void *arg,
+                    unsigned long int addr,
+                    const char *buf,
+                    size_t count) {
+    mem->write(arg, addr, buf, count);
+}
+
+void rpc_EraseCommand(struct mem_Device *mem, void *arg) {
+    mem->erase(arg); 
+}
+
--- src/app/rpc_server.c
+++ src/app/rpc_server.c
@@ -1,61 +1,59 @@
 #include "app/rpc_server.h"
 
-static void default_handle_request_cb(struct rpc_Server *com);
-static void (*handle_request_cb)(struct rpc_Server *com) = &default_handle_request_cb;
-static void default_idle_cb(struct rpc_Server *com);
-static void (*idle_cb)(struct rpc_Server *com) = &default_idle_cb; 
+static void default_handle_request_cb(struct rpc_Server *server);
+static void (*handle_request_cb)(struct rpc_Server *server) = &default_handle_request_cb;
+static void default_idle_cb(struct rpc_Server *server);
+static void (*idle_cb)(struct rpc_Server *server) = &default_idle_cb; 
 
-void rpc_ServerTick(struct rpc_Server *com) {
-    if (com->status == RPC_REQUEST_PENDING) {
-        handle_request_cb(com);
+void rpc_ServerTick(struct rpc_Server *server) {
+    if (server->status == RPC_REQUEST_PENDING) {
+        handle_request_cb(server);
     } else {
-        idle_cb(com);
+        idle_cb(server);
     } 
 }
 
-
-
-void rpc_SetServerIdleCallback(void (*cb)(void)) {
+void rpc_SetServerIdleCallback(void (*cb)(struct rpc_Server *)) {
     if (cb) {
         idle_cb = cb;
     }
 }
-void rpc_ServerRequest(struct rpc_Server *com) {
-    com->status = RPC_REQUEST_PENDING;
+void rpc_ServerRequest(struct rpc_Server *server) {
+    server->status = RPC_REQUEST_PENDING;
 }
-int rpc_RequestPending(struct rpc_Server *com) {
-    return com->status == RPC_REQUEST_PENDING;
-}
-
-enum rpc_Status rpc_GetStatus(struct rpc_Server *com) {
-    return com->status;
+int rpc_RequestPending(struct rpc_Server *server) {
+    return server->status == RPC_REQUEST_PENDING;
 }
 
-void rpc_WriteBuffer(struct rpc_Server *com, const char *buf, size_t count) {
+enum rpc_Status rpc_GetStatus(struct rpc_Server *server) {
+    return server->status;
+}
+
+void rpc_WriteBuffer(struct rpc_Server *server, const char *buf, size_t count) {
     if (count > RPC_BUFFER_SIZE) {
-        com->status = RPC_BUFFER_OVERFLOW;
+        server->status = RPC_BUFFER_OVERFLOW;
     } else {
         for (size_t i = 0; i < count; i++) {
-            com->buf[i] = buf[i];
+            server->buf[i] = buf[i];
         }
     }
 }
 
-void rpc_ReadBuffer(struct rpc_Server *com, char *buf, size_t count) {
+void rpc_ReadBuffer(struct rpc_Server *server, char *buf, size_t count) {
     if (count > RPC_BUFFER_SIZE) {
-        com->status = RPC_BUFFER_OVERFLOW;
+        server->status = RPC_BUFFER_OVERFLOW;
     } else {
         for (size_t i = 0; i < count; i++) {
-            buf[i] = com->buf[i];
+            buf[i] = server->buf[i];
         }
     }
 }
 
-static void  default_handle_request_cb(struct rpc_Server *com) {
+static void  default_handle_request_cb(struct rpc_Server *server) {
     return;
 }
 
-static void default_idle_cb(struct rpc_Server *com) {
+static void default_idle_cb(struct rpc_Server *server) {
     /* go to sleep */
     return;
 }
